649d66a4554bcbf2d3172f803fa5b103a7f007c9
Gina Beford <gina@MyAir.local>
2017-11-15 21:00:10 -0500

    set up component shells

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TrackerComponent } from './tracker/tracker.component';
+import { ReportsComponent } from './reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackerComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule
