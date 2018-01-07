5a63e5079b5d164ff64ca02eeea3e71e5426dd62
John Tsombakos <johnt519@gmail.com>
2018-01-06 22:03:55 -0500

    Use sandbox for local development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase_467_timeTracker: {
-    apiKey: 'AIzaSyDLKFASaeQE14QdTQ7CdmD1D7nFxas4gcc',
-    authDomain: 'time-tracker-467.firebaseapp.com',
-    databaseURL: 'https://time-tracker-467.firebaseio.com',
-    projectId: 'time-tracker-467',
-    storageBucket: 'time-tracker-467.appspot.com',
-    messagingSenderId: '220924576055'
+    apiKey: 'AIzaSyDtP3U8t0hKvhUdoRtLbT2CNuB5aVL3zEY',
+    authDomain: 'timetrackersandbox.firebaseapp.com',
+    databaseURL: 'https://timetrackersandbox.firebaseio.com',
+    projectId: 'timetrackersandbox',
+    storageBucket: 'timetrackersandbox.appspot.com',
+    messagingSenderId: '843771130494'
   }
 };
