e518be87a2acbde2416afa201251d114ea1b7cfc
Gina Beford <ginabeford@Ginas-iMac.local>
2018-02-07 12:26:13 -0500

    add sass files and start modifying css

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,7 +6,7 @@ import { TimeTrackerService } from 'app/time-tracker/time-tracker.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
 
