0014661829e475bfbe248556b7aa399eb7687c82
Gina Beford <ginabeford@Ginas-iMac.local>
2018-02-14 16:49:30 -0500

    add message to student list

--- src/app/forms/badge/badge-view/badge-view.component.ts
+++ src/app/forms/badge/badge-view/badge-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IStudent } from '../../../model/student';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-badge-view',
@@ -8,7 +9,7 @@ import { IStudent } from '../../../model/student';
 })
 export class BadgeViewComponent implements OnInit {
   studentID = '';
-
+  public badgeForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
