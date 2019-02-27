83638abe13f7c3fd889f5ae3cba7ad3b819c307d
Gina Beford <gbeford@gmail.com>
2019-02-27 10:31:02 -0500

    add failed unit test to student

--- src/app/student/student.service.spec.ts
+++ src/app/student/student.service.spec.ts
@@ -36,9 +36,6 @@ describe('MessageService', () => {
                 email: 'jdoe@shrewsburyrobotics.org',
                 grade: 10,
                 isSignedIn: false,
-                // updated?: Date,
-                // created?: Date,
-                // signInTime?: Date,
                 messages: ['Go see Carol', 'Sign up for emails'],
                 eventID: 1
             },
@@ -89,5 +86,32 @@ describe('MessageService', () => {
 
             expect(response).toEqual(studentResponse);
         });
+
+        fit('should fail due to trying to access wrong id', () => {
+            const studentResponse = {
+                id: 12,
+                studentId: 122321,
+                firstName: 'John',
+                lastName: 'Doe',
+                email: 'jdoe@shrewsburyrobotics.org',
+                grade: 10,
+                isSignedIn: false,
+                created: '2/21/19',
+                signInTime: '2/21/19',
+                messages: ['Go see Carol', 'Sign up for emails'],
+                eventID: 1
+            };
+            let response;
+
+            spyOn(studentService, 'getStudent').and.returnValue(of(studentResponse));
+
+            studentService.getStudent('4').subscribe(res => {
+                response = res;
+            });
+
+            expect(response).toEqual(studentResponse);
+        });
+
+
     });
 });
