c66573c0f5e8b1ec9fee461f63dd93dbf944f8eb
Gina Beford <gbeford@gmail.com>
2019-12-20 15:40:47 -0500

    clean up code

--- src/app/apparel/apparel-store-front/apparel-store-front.component.ts
+++ src/app/apparel/apparel-store-front/apparel-store-front.component.ts
@@ -44,17 +44,13 @@ export class ApparelStoreFrontComponent implements OnInit {
     private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
-    // this.getSizes();
-    // this.getImageList();
-    this.getAppareal();
+     this.getAppareal();
     this.createForm();
     this.getCartDetails();
     this.getCart();
   }
 
-  // getSizes() {
-  //   this.sizes = this.clothingService.getClothingSize();
-  // }
+
 
   // getImageList() {
   //   this.clothingService.getImages().subscribe(data => {
@@ -82,58 +78,10 @@ export class ApparelStoreFrontComponent implements OnInit {
 
   createForm() {
     this.form = this.formBuilder.group({
-  //     itemCtrl: [''],
-  //     descCtrl: [''],
-  //     typeCtrl: [''],
-  //     upChargeCtrl: [''],
-  //     priceCtrl: [''],
-  //     nameChargeCtl: [''],
-  //     canHaveNameCtl: [''],
-  //     genderCtrl: ['', Validators.required],
-  //     quantityCtrl: ['', Validators.required],
-  //     size: ['', Validators.required],
-  //     sleeveNameCtrl: [''],
-    });
+      });
   }
 
-  // convenience getter for easy access to form fields
-  // get f() { return this.apparelForm.controls; }
-
-
-  // public addItemToCart(apparel: IApparel) {
-  //   this.submitted = true;
-
-  //   // stop here if form is invalid
-  //   if (this.apparelForm.invalid) {
-  //     return;
-  //   }
-
-  //   // validate if checkbox for name is selected that a name is in the name input
-  //   if (this.apparelForm.value.canHaveNameCtl === true &&
-  //     (this.apparelForm.value.sleeveNameCtrl === null || this.apparelForm.value.sleeveNameCtrl === '')) {
-  //     this.hasError = true;
-  //     return this.apparelForm.invalid === false;
-
-  //   }
-
-  //   // add apparel item to model
-  //   this.item = new CartItem();
-  //   this.item.apparel = apparel;
-  //   this.item.upCharge = this.apparelForm.value.upChargeCtrl ? this.apparelForm.value.upChargeCtrl : null;
-  //   this.item.nameCharge = this.apparelForm.value.nameChargeCtl ? this.apparelForm.value.nameChargeCtl : null;
-  //   this.item.gender = this.apparelForm.value.genderCtrl;
-  //   this.item.size = this.apparelForm.value.size;
-  //   this.item.quantity = this.apparelForm.value.quantityCtrl;
-  //   this.item.sleeveName = this.apparelForm.value.sleeveNameCtrl;
-  //   this.item.nameOnSleeve = this.apparelForm.value.canHaveNameCtl;
-
-  //   this.shoppingCartService.addItem(this.item);
-
-  //   this.showBox = true;
-  //   this.apparelForm.reset();
-  //   this.submitted = false;
-  // }
-
+  
 
   // get last item added to the cart, (subscribe to behavior subject)
   getCartDetails() {
